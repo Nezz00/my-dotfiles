ab8cd830c880f0fe64334151ffe409464a0ae9ea
Nezz00 <nezztel@gmail.com>
2025-07-04 15:08:21 -0300

    Add files via upload

---
+++ dotfiless/config/waybar/scripts/wlogout.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+hyprctl dispatch movetoworkspacesilent special
+wlogout --layout /usr/share/wlogout/layout_vertical.lua --buttons-per-row 1 --column-spacing 50 --row-spacing 50 --x-offset -200 --y-offset 50
